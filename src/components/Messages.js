e132056e55576f356bcd454e7417eb971fac947c
Marko Radevenjic <marko.radevenjic@gmail.com>
2023-09-06 21:26:55 +0200

    App.css modified and Messages.js added

---
+++ src/components/Messages.js
@@ -0,0 +1,44 @@
+import {useEffect, useRef} from 'react';
+import React from 'react';
+
+function Messages({messages, me}) {
+    const bottomRef = useRef(null);
+
+    useEffect(() => {
+        if (bottomRef && bottomRef.current) {
+            bottomRef.current.scrollIntoView({behavior: 'smooth'});
+        }
+    });
+
+    function Message({member, data, id}, me) {
+        const {username, color} = member.clientData;
+        const messageFromMe = member.id === me.id;
+        const className = messageFromMe ? "messages-message current-member" : "messages-message";
+
+        return (
+            <li key={id} className={className}>
+                <span
+                    className="avatar"
+                    style={{backgroundColor: color}}
+                />
+                <div className="message-content">
+                    <div className="username">
+                        {username}
+                    </div>
+                    <div className="text">
+                        {data}
+                    </div>
+                </div>
+            </li>
+        );
+    }
+
+    return (
+        <ul className="messages-list">
+            {messages.map(m => Message(m, me))}
+            <div ref={bottomRef}></div>
+        </ul>
+    );
+}
+
+export default Messages;
